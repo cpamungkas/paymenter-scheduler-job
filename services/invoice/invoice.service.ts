1487180e477bd408df95dac730a9e81303f29153
Kang CP <necha10.02@gmail.com>
2025-03-30 00:25:10 +0700

    Update invoice.service.ts

--- services/invoice/invoice.service.ts
+++ services/invoice/invoice.service.ts
@@ -1,6 +1,9 @@
 import type { InvoicesResponse } from "../../dtos/invoice";
 import type { Invoice } from "../../models/invoice";
 
+const BaseURL = process.env.BASE_URL;
+const UserToken = process.env.USER_TOKEN;
+
 console.log("BASE_URL:", process.env.BASE_URL || "Not Found");
 console.log("USER_TOKEN:", process.env.USER_TOKEN ? "Exists" : "Not Found");
 
